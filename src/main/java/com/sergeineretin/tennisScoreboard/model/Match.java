20f544ebb9666e1166f0e0c2d8a366d1cd68e11f
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-07-20 01:45:46 +0700

    fix: hibernate.cfg.xml is fixed

--- src/main/java/com/sergeineretin/tennisScoreboard/model/Match.java
+++ src/main/java/com/sergeineretin/tennisScoreboard/model/Match.java
@@ -1,11 +1,11 @@
 package com.sergeineretin.tennisScoreboard.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 
 @Entity
 @Table(name = "Matches")
@@ -19,12 +19,12 @@ public class Match {
     @Column(name = "ID")
     private int id;
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
-    @JoinColumn(name = "Player1", foreignKey = @ForeignKey(name = "FK"))
+    @JoinColumn(name = "Player1", foreignKey = @ForeignKey(name = "FK_Player1"))
     private Player player1;
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
-    @JoinColumn(name = "Player2", foreignKey = @ForeignKey(name = "FK"))
+    @JoinColumn(name = "Player2", foreignKey = @ForeignKey(name = "FK_Player2"))
     private Player player2;
-    @JoinColumn(name = "Winner", foreignKey = @ForeignKey(name = "FK"))
+    @JoinColumn(name = "Winner", foreignKey = @ForeignKey(name = "FK_Winner"))
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
     private Player winner;
 }
--- src/main/java/com/sergeineretin/tennisScoreboard/model/Player.java
+++ src/main/java/com/sergeineretin/tennisScoreboard/model/Player.java
@@ -1,16 +1,10 @@
 package com.sergeineretin.tennisScoreboard.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "Players")
